33a6653ea733f67bfac324c8eccaf9431bcb4dc5
ws0671 <ws0671@naver.com>
2022-10-03 20:47:37 +0900

    Feat: controllers

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { edit, see, upload, deleteVideo } from "../controllers/videoController";
+
+const videoRouter = express.Router();
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
+
+export default videoRouter;
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000;
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () => {
+  console.log(`Server is listening http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
