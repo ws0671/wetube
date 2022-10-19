0d73ae78d90bd4cddb4d7d53e5b90f4d525e56fd
ws0671 <ws0671@naver.com>
2022-10-19 23:20:51 +0900

    Feat: edit video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { edit, see, upload, deleteVideo } from "../controllers/videoController";
+import {
+  getEdit,
+  watch,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
-
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -4,17 +4,14 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () => {
-  console.log(`Server is listening http://localhost:${PORT}`);
-};
-
-app.listen(PORT, handleListening);
+export default app;
