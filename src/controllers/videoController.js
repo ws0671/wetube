228d678fdc9edf1c6385828d0e233ab67b5c91d2
ws0671 <ws0671@naver.com>
2022-10-31 16:04:01 +0900

    6.26 Search part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -61,3 +61,12 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  console.log(req.query);
+  if (keyword) {
+    //search
+  }
+  return res.render("search", { pageTitle: "Search" });
+};
