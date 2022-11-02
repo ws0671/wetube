25d7488a2a464a573532478905e8fe2019525729
ws0671 <ws0671@naver.com>
2022-11-02 14:53:00 +0900

    7.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -10,6 +11,22 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+// session 미들웨어는 반드시 router앞에서 초기화한다.
+app.use(
+  session({
+    secret: "hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+//router
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
