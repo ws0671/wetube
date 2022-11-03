030246824fd07fb162120037091651175d4146ce
ws0671 <ws0671@naver.com>
2022-11-03 22:18:03 +0900

    7.20 Github Login part Five

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -95,18 +95,31 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
-  // 1. 위에서 받은 code를 이용해 access_token을 가져온다.
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
-          // 2. access_token으로 유저정보를 가져온다.
           Authorization: `Bearer ${access_token}`,
         },
       })
     ).json();
-    console.log(userRequest);
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
+    const email = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!email) {
+      return res.redirect("/login");
+    }
   } else {
     return res.redirect("/login");
   }
