3bfd8cc777a9b19482bf9dde1ba6e713aec59924
ws0671 <ws0671@naver.com>
2022-11-07 22:09:37 +0900

    8.1 Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,16 @@
 export const localsMiddleware = (req, res, next) => {
-  // 세션에 저장된 로그인 정보가 true또는 false이면 locals에도 로그인 정보를 기록한다.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  // 세션에 저장된 유저정보를 locals에도 저장한다.
   res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) return next();
+  return res.redirect("/");
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) return next();
+  return res.redirect("/");
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,14 +8,15 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
