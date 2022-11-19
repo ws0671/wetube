cc1a95da5478e25ad96331bb0174cb9b2b8667c8
ws0671 <ws0671@naver.com>
2022-11-19 15:36:14 +0900

    11.4 Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,10 +1,14 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumn = document.getElementById("volumn");
+const volumeRange = document.getElementById("volume");
 
-console.log(video);
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
@@ -14,14 +18,44 @@ const handlePlayClick = (e) => {
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMute = (e) => {
+const handleMuteClick = (e) => {
   if (video.muted) {
     video.muted = false;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  // muteBtn을 눌러서 muted가 true이면 그 상태에서 volume을 조절해도 계속 true
+  // 따라서 muted가 true일때 volume을 조절하면 muted가 false가 되고
+  // muted가 아닌 상태가 되기 때문에, muteBtn에 "Mute" 글자를 넣어준다.
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+const handleLoadedMetadata = () => {
+  //duration 속성은 video의 총 시간을 초단위로 가져온다.
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+// meta data가 로드될 때 실행된다. 여기서 meta data는 비디오를 제외한 정보를 말한다.
+// 비디오의 크기, 비디오의 길이
+// 즉, 비디오에서 움직이는 이미지들을 제외한 모든 엑스트라 정보들을 말한다.
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+// 시간이 업데이트 될때마다 실행되는 이벤트
+video.addEventListener("timeupdate", handleTimeUpdate);
