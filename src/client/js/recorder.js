b1b9f0e49bbb8a61cacc8047b3a5da8f0ff48df2
ws0671 <ws0671@naver.com>
2022-11-22 07:59:59 +0900

    14.2 Download Transcoded Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -10,11 +10,17 @@ const handleDownload = async () => {
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  // webm파일을 output.mp4로 변형시킨다.
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  // 브라우저 메모리에 저장된 output.mp4를 읽는다.
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const mp4Url = URL.createObjectURL(mp4Blob);
 
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  // a태그 경로에 매직url(mp4Url)을 연결시킨다.
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
 };
