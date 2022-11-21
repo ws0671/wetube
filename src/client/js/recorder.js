7b2ba6e483c35d1046cb15e908a5b58b2834c19e
ws0671 <ws0671@naver.com>
2022-11-21 13:48:30 +0900

    13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,13 @@
 const startBtn = document.getElementById("startBtn");
-
+const video = document.getElementById("preview");
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  // srcObject속성은 HTMLMediaElement와 연결된 미디어의 소스 역할을 하는 객체를 설정하거나
+  // 반환한다.
+  video.srcObject = stream;
+  video.play();
 };
 startBtn.addEventListener("click", handleStart);
