cbeb00e96e9e91d05a3f7017f87f7f8f20eb0631
ws0671 <ws0671@naver.com>
2022-11-21 13:31:27 +0900

    13.0 Recorder Setup

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,3 @@
+// 1. main.js에서 regenerator-runtime을 불러온다.
+import regeneratorRuntime from "regenerator-runtime";
 import "../scss/styles.scss";
-
-// console.log("main");
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   },
   output: {
     filename: "js/[name].js",
