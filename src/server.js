54446ee10796b3a35dd4bb14f0eaf7412be9aa1a
ws0671 <ws0671@naver.com>
2022-12-05 00:52:14 +0900

    Fix: server.js

--- src/server.js
+++ src/server.js
@@ -16,14 +16,12 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use((req, res, next) => {
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
