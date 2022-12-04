b24ffb56e3e1f075852bf75b6cc99a1656584a67
ws0671 <ws0671@naver.com>
2022-12-05 00:29:57 +0900

    Fix: server.js

--- src/server.js
+++ src/server.js
@@ -16,8 +16,12 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Access-Control-Allow-Headers");
+  res.header("Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(logger);
