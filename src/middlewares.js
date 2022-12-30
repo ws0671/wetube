0b2c984e1b6a78e8d9c1a6077eaf3e53de288b72
ws0671 <ws0671@naver.com>
2022-12-30 11:28:08 +0900

    Feat: isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -39,9 +39,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
   res.locals.isHeroku = isHeroku;
-  // console.log(res.locals.loggedInUser.subscribes);
-  // console.log(res.locals.loggedInUser.subscribes.avatarUrl);
-  // console.log(res.locals.loggedInUser.subscribes.name);
+  console.log(res.locals.isHeroku);
   next();
 };
 
